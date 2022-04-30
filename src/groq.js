687e9d43b7b201690c121ae0d78da8331a51684a
Derek Nguyen <dikanguok@gmail.com>
2022-04-30 23:08:42 +0900

    try fetching

--- src/groq.js
+++ src/groq.js
@@ -1,18 +1,8 @@
-import path from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
 import { evaluate, parse } from 'groq-js';
 
-const require = createRequire(import.meta.url);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const file_data = path.join(__dirname, '/data/data.json');
-const data = require(file_data);
-
-function query(input) {
+function query(dataset, input) {
 	const tree = parse(input);
-	return evaluate(tree, { dataset: data });
+	return evaluate(tree, { dataset });
 }
 
 export default query;
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,9 +2,11 @@ import query from '~/groq';
 import chart_config from '~/chart_config';
 
 function assemble_filter({ field_name, value }) {
-	const filter_config = chart_config[field_name].filter
+	const filter_config = chart_config[field_name].filter;
 	if (!filter_config || !filter_config.type) return null;
-	return `"${value}" ${filter_config.type === 'equal' ? '==' : 'in'}  ${field_name}`;
+	return `"${value}" ${
+		filter_config.type === 'equal' ? '==' : 'in'
+	}  ${field_name}`;
 }
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
@@ -12,12 +14,18 @@ export async function post({ request }) {
 	const body = await request.json();
 	const { chart_ids, filters } = body;
 
-	const q_filter = filters.map(_filter => assemble_filter(_filter)).filter((v) => v).join(' && ')
+	const dataset = await fetch('/data.json').then((res) => res.json());
+
+	const q_filter = filters
+		.map((_filter) => assemble_filter(_filter))
+		.filter((v) => v)
+		.join(' && ');
 
 	try {
 		const tasks = chart_ids.map(async (id) => {
 			const { field_name, transform, unit } = chart_config[id];
 			const result = await query(
+				dataset,
 				`*[${q_filter}] { "field": ${field_name} }`
 			);
 			const raw = await result.get();
