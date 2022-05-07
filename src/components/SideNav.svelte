b29befa761f51975ed27154331d2a9d7589cbcea
Derek Nguyen <dikanguok@gmail.com>
2022-05-08 01:02:12 +0900

    feat: add basic nav

---
+++ src/components/SideNav.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { page } from '$app/stores';
+
+	const navs = [
+		{ id: '0', path: '#', label: 'Welcome' },
+		{ id: '1', path: '/participants', label: 'Participant info' },
+		{ id: '2', path: '/industry-info', label: 'Industry Info' }
+	];
+
+	$: current_page = $page.url.pathname;
+</script>
+
+<nav>
+	<ul class="nav-list">
+		{#each navs as { id, path, label, prefetch = true } (id)}
+			<li class="nav-item">
+				<a
+					class:active={current_page === path}
+					sveltekit:prefetch={prefetch}
+					href={path}>{label}</a
+				>
+			</li>
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	.nav-list {
+		margin: 0;
+		padding: 0;
+		list-style-type: none;
+        margin-top: 1.25rem;
+	}
+
+	.nav-item {
+		display: block;
+	}
+
+	.nav-item a {
+        display: block;
+        color: currentColor;
+		padding: 0.5rem 1rem 0.75rem;
+		text-decoration: none;
+		font-size: 1.125rem;
+		border-left: 2px solid transparent;
+	}
+
+	.nav-item a.active {
+		border-color: var(--color-chart-bar-fg);
+		background-color: var(--color-chart-bar-bg);
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,37 @@
 <script>
 	import FilterBar from '~/components/FilterBar.svelte';
+	import SideNav from '~/components/SideNav.svelte';
 	import 'modern-normalize/modern-normalize.css';
-    import '~/global.css';
+	import '~/global.css';
 </script>
 
-<div class="container">
-	<slot />
+<div class="layout container">
+	<div class="layout-sidenav">
+		<SideNav />
+	</div>
+	<div class="layout-content">
+		<slot />
+	</div>
 </div>
 
 <FilterBar />
 
 <style>
+	@media (min-width: 400px) {
+		.layout {
+			display: flex;
+			gap: 2rem;
+		}
+
+		.layout-content {
+			flex: 1;
+		}
+
+		.layout-sidenav {
+			width: 20%;
+			align-self: flex-start;
+			top: 0;
+			position: sticky;
+		}
+	}
 </style>
