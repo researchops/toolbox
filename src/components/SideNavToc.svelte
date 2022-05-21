a43aab41f5473453f5db89897b8fa2ec0980598b
Derek Nguyen <dikanguok@gmail.com>
2022-05-21 23:48:19 +0900

    feat: remove h3 from side nav

--- src/components/SideNavToc.svelte
+++ src/components/SideNavToc.svelte
@@ -5,16 +5,18 @@
 {#if toc.length > 0}
 	<ul class="toc-list">
 		{#each toc as item, i (i)}
-			<li class="toc-item" data-toc-level={item.level}>
-				<a href={item.url}>{item.text}</a>
-			</li>
+			{#if item.level < 2}
+				<li class="toc-item">
+					<a href={item.url}>{item.text}</a>
+				</li>
+			{/if}
 		{/each}
 	</ul>
 {/if}
 
 <style>
 	.toc-list {
-		margin: .5rem 0 0 2rem;
+		margin: 0.5rem 0 0 2rem;
 		padding: 0;
 		list-style-type: none;
 	}
