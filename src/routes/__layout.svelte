50930ab662538b312f8cbaf9b336f84c7c194221
andypbrowne <andypbrowne@gmail.com>
2022-05-27 14:14:29 -0400

    remove the 700 wweight too because the font weight change is not useful on hover, makes thing jump around

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,6 @@
 	import '@fontsource/source-sans-pro/400.css';
 	import '@fontsource/source-sans-pro/400-italic.css';
 	import '@fontsource/source-sans-pro/600.css';
-	import '@fontsource/source-sans-pro/800.css';
 	import '~/global.css';
 </script>
 
