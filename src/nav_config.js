7df3c2999e6de8289cb120a33309597cef80e68e
Derek Nguyen <dikanguok@gmail.com>
2022-06-05 23:42:13 +0900

    fix: recover nav

--- src/nav_config.js
+++ src/nav_config.js
@@ -1,4 +1,46 @@
 export const nav_config = [
 	{ id: '0', path: '/', label: 'Welcome' },
 	{ id: '1', path: '/about', label: 'About the project' },
+	{ id: '2', path: '/participants', label: 'Participant info' },
+	{ id: '3', path: '/industry-info', label: 'Industry info' },
+	{
+		id: '4',
+		path: '/tools-operations-planning',
+		label: 'Operations and planning'
+	},
+	{
+		id: '5',
+		path: '/participant-management',
+		label: 'Recruiting & Participant Management'
+	},
+	{
+		id: '6',
+		path: '/design-collaboration',
+		label: 'Design & Collaboration'
+	},
+	{
+		id: '7',
+		path: '/analytics',
+		label: 'Analytics'
+	},
+	{
+		id: '8',
+		path: '/design-research',
+		label: 'Design Research'
+	},
+	{
+		id: '9',
+		path: '/user-research',
+		label: 'User Research'
+	},
+	{
+		id: '10',
+		path: '/synthesizing-analyzing',
+		label: 'Synthesizing & analyzing'
+	},
+	{
+		id: '11',
+		path: '/reporting-sharing',
+		label: 'Reporting & sharing'
+	}
 ];
