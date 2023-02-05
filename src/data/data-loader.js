e6c7dc36a5bc51526112affe19d28c5258cc5395
Derek Nguyen <dikanguok@gmail.com>
2023-02-06 04:15:01 +0900

    Revert "fix: break cache & rename file"
    This reverts commit 204cfd1242e483df83e9cdfec56097471a9c8ca6.

--- src/data/data-loader.js
+++ src/data/data-loader.js
@@ -1,5 +1,5 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-const data = require('./data-20230206.json');
+const data = require('./data.json');
 export default data
